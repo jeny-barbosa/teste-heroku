6d4387978ddb3b56d73784934b9d9d5eef79e8bb
Jennefer <51484263+jeny-barbosa@users.noreply.github.com>
2019-06-12 14:50:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,3 @@ echo '<h1>Olá Mundo! </h1>';
 echo '<h2>meu primeiro projeto online </h2>';
 
 echo '<h6>Jennefer Barbosa </h6>';
-
-<form> 
-  Nome: <input type="text" name="nome" placeholder="Fulano de Tal">
-  </form>
